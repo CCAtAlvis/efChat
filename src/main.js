00624bbf0ae90881fc21990c4aaa9f8e7ebf00a3
Chinmay Chandak <chandakchinmay@gmail.com>
2017-11-13 17:09:49 +0530

    settin up initial project UI
    integrated firebase properly
    added dependency for jquery and bcrypt
    
    Signed-off-by: Chinmay Chandak <chandakchinmay@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,26 +1,52 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const url = require('url')
-const admin = require('firebase-admin')
-const functions = require('firebase-functions')
+// declearing electron modules
+const electron = require("electron");
+const { app, BrowserWindow } = electron;
+const path = require("path");
+const url = require("url");
+
+// declearing firebase modules 
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+
+// getting the credential of firebase project
+const firebaseCredentials = require("./firebase.json");
+
+// initializing firebase app.
+// replace databaseURL with URL of you database
+admin.initializeApp({
+    credential: admin.credential.cert(firebaseCredentials),
+    databaseURL: "https://efchat-cb07c.firebaseio.com/"
+});
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
-function createWindow() {
+function createWindow ()
+{
+    // create the browser window option
+    let bwOption = {
+        // transparent makes the title bar transparent
+        // other wise title bar will take system default color
+        transparent: true,
+        width: 300,
+        minWidth: 300,
+        // maxWidth: 300,
+   
+        height: 400,
+        minHeight: 400
+        // maxHeight: 400
+        // useContentSize: true
+    };
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow( bwOption );
 
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
         slashes: true
-    }))
-
-    // Open the DevTools.
-    win.webContents.openDevTools()
+    }));
 
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -28,30 +54,36 @@ function createWindow() {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         win = null
-    })
+    });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (process.platform !== 'darwin')
+    {
         app.quit()
     }
-})
+});
 
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow()
-    }
-})
+    if (win === null)
+        createWindow();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// set the menu bar to null when the browser window is created
+// so we have a simless effect
+app.on('browser-window-created', (e, window) => {
+    window.setMenu(null);
+});
