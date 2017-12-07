ced49fbd83b85188b867f80db7f2d49cea7b9e90
CCAtAlvis <chandakchinmay@gmail.com>
2017-12-08 01:10:08 +0530

    Standardisation
    that explains for it self
    but still...
    edited my code to follow js standards

--- src/js/index.js
+++ src/js/index.js
@@ -1,169 +1,156 @@
 // including jquery
-const $ = require("jquery");
+const $ = require('jquery');
 
 // declearing firebase modules
-const admin = require("firebase-admin");
-const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
 
 // getting the credential of firebase project
-const firebaseCredentials = require("./firebase.json");
+const firebaseCredentials = require('./firebase.json');
 
 // initializing firebase app.
 // replace databaseURL with URL of you database
 admin.initializeApp({
-    credential: admin.credential.cert(firebaseCredentials),
-    databaseURL: "https://efchat-cb07c.firebaseio.com/"
+	credential: admin.credential.cert(firebaseCredentials),
+	databaseURL: 'https://efchat-cb07c.firebaseio.com/'
 });
 
-$("input[type=button]").on( "click", ( e ) => {
-    // prevent the default action of the event
-    e.preventDefault();
+$('input[type=button]').on('click', (e) => {
+	// prevent the default action of the event
+	e.preventDefault();
 
-    // get the username and password from their respetive field
-    let username = $( "#username" ).val();
-    let password = $( "#password" ).val();
+	// get the username and password from their respetive field
+	let username = $('#username').val();
+	let password = $('#password').val();
 
-    // remove all the whitespaces
-    username = username.replace( /\s/g, "" );
-    password = password.replace( /\s/g, "" );
+	// remove all the whitespaces
+	username = username.replace(/\s/g, '');
+	password = password.replace(/\s/g, '');
 
-    // console.log( e.target );
+	// console.log( e.target );
 
-    let type = $(e.target).attr("name");
-    // console.log( type );
-    // console.log( username + " : " + password );
+	let type = $(e.target).attr('name');
+	// console.log( type );
+	// console.log( username + ' : ' + password );
 
-    // if both the username and password are not empyt
-    // lets check if the user wants to login or signup
-    if ( "" !== username && "" !== password )
-    {
-        // if the user wants to login
-        // get the user password from the firebase and compare it with the user entered password 
-        if ( "login" === type )
-        {
-            // console.log( "starting login process" );
+	// if both the username and password are not empyt
+	// lets check if the user wants to login or signup
+	if ('' !== username && '' !== password) {
+		// if the user wants to login
+		// get the user password from the firebase and compare it with the user entered password
+		if ('login' === type) {
+			// console.log( 'starting login process' );
 
-            // 1: fetch the password from firebase
-            // 2: compare it with user entered password
-            // if matches
-                // login user
-            // if not
-                // alert user about wrong password
-            // if returned null object
-            // alert user that the account does not exist
-            
-            admin.database().ref( `users/${username}` ).once( "value" ).then( ( snapshot ) => {
-                // fetch the password and store
-                let fetchedPassword = snapshot.val();
-                // console.log( fetchedPassword + "     " + typeof fetchedPassword );
-                // password     string
-                // null     object
+			// 1: fetch the password from firebase
+			// 2: compare it with user entered password
+			// if matches
+			// login user
+			// if not
+			// alert user about wrong password
+			// if returned null object
+			// alert user that the account does not exist
 
-                if ( "object" === typeof fetchedPassword )
-                {
-                    // alert user that the account des not exist
+			admin.database().ref(`users/${username}`).once('value').then((snapshot) => {
+				// fetch the password and store
+				let fetchedPassword = snapshot.val();
+				// console.log( fetchedPassword + '     ' + typeof fetchedPassword );
+				// password     string
+				// null     object
 
-                }
-                else if ( "string" === typeof fetchedPassword )
-                {
-                    // compare the passwords
+				if ('object' === typeof fetchedPassword) {
+					// alert user that the account des not exist
 
-                    if ( fetchedPassword === password )
-                    {
-                        // Yes! the passwords match, log the user in
-                        // console.log( "logged in!!!" );
-                    }
-                    else
-                    {
-                        // the passwords doesn't match
-                        // alert the user
-                    }
-                }
-            });    
-        }
-        // if the user wants to signup
-        else if ( "signup" === type )
-        {
-            // console.log( "starting signup process" );
+				}
+				else if ('string' === typeof fetchedPassword) {
+					// compare the passwords
 
-            // 1: add another password field for confirm password
-            // 2: get that password
-            // 3: check if both the passwords are same
-            // if so 
-                // check if the username exist or not
-                // if exist
-                    // alert user to change username
-                // if not
-                    // insert the user details into firebase
-                    // login the user
-            // if not
-                // alert the user
+					if (fetchedPassword === password) {
+						// Yes! the passwords match, log the user in
+						// console.log( 'logged in!!!' );
+					}
+					else {
+						// the passwords doesn't match
+						// alert the user
+					}
+				}
+			});
+		}
+		// if the user wants to signup
+		else if ('signup' === type) {
+			// console.log( 'starting signup process' );
 
-            // heres what we want to do
-            // if the user clicks on signup for first time show the confirm password box
-            // when they click again we will check both the passwords and follow the above steps
-            // 
-            // and heres who we do that
-            // so first lets get the value of the #confirm-password
-            // if #confirm-password does not exist then it will return unddefined
-            // which means we have to create the #confirm-password input box
-            // if it returnes a value, whether it be empyt that means, input box is creadted
-            // and we need to check that value with password field
+			// 1: add another password field for confirm password
+			// 2: get that password
+			// 3: check if both the passwords are same
+			// if so
+			// check if the username exist or not
+			// if exist
+			// alert user to change username
+			// if not
+			// insert the user details into firebase
+			// login the user
+			// if not
+			// alert the user
 
-            // get the confirm password
-            let confirmPassword = $( "#confirm-password" ).val();
+			// heres what we want to do
+			// if the user clicks on signup for first time show the confirm password box
+			// when they click again we will check both the passwords and follow the above steps
+			//
+			// and heres who we do that
+			// so first lets get the value of the #confirm-password
+			// if #confirm-password does not exist then it will return unddefined
+			// which means we have to create the #confirm-password input box
+			// if it returnes a value, whether it be empyt that means, input box is creadted
+			// and we need to check that value with password field
 
-            // console.log( confirmPassword );
+			// get the confirm password
+			let confirmPassword = $('#confirm-password').val();
 
-            // if the 
-            if ( undefined !== confirmPassword )
-            {
-                // get the confirmPassword
-                confirmPassword = confirmPassword.replace( /\s/g, "" );
+			// console.log( confirmPassword );
 
-                // check whether password and confirmPassword match
-                if ( confirmPassword === password )
-                {
-                    // now that the passwords match
-                    // get a snapshot of firebase with username
-                    
-                    admin.database().ref( `users/${username}` ).once( "value" ).then( ( snapshot ) => {
-                        // fetch the password and store
-                        let fetchedPassword = snapshot.val();
-                        // console.log( fetchedPassword + "     " + typeof fetchedPassword );
-                        // password     string
-                        // null     object
+			// if the
+			if (undefined !== confirmPassword) {
+				// get the confirmPassword
+				confirmPassword = confirmPassword.replace(/\s/g, '');
 
-                        // snapshot.va() will return a
-                        // string: if the user exis
-                        // object (null): if it doesn't
+				// check whether password and confirmPassword match
+				if (confirmPassword === password) {
+					// now that the passwords match
+					// get a snapshot of firebase with username
 
-                        if ( "object" === typeof fetchedPassword )
-                        {
-                            // user does not exist
-                            // lets create a new one.
+					admin.database().ref(`users/${username}`).once('value').then((snapshot) => {
+						// fetch the password and store
+						let fetchedPassword = snapshot.val();
+						// console.log( fetchedPassword + '     ' + typeof fetchedPassword );
+						// password     string
+						// null     object
 
-                            admin.database().ref( `users/${username}` ).set( password );
+						// snapshot.va() will return a
+						// string: if the user exis
+						// object (null): if it doesn't
 
-                            // now alert user that their account has been created
-                        }
-                        else if ( "string" === typeof fetchedPassword )
-                        {
-                            // a user already exists
-                            // alert the user to change username
-                        }
-                    });
-                }
-                else
-                {
-                    // alert the user the passwords are not the same
-                }
-            }
-            else
-            {
-                $("#password-fileds").append( "<input type=password id=confirm-password placeholder=\"Confirm Password *\">" );
-            }
-        }
-    }
+						if ('object' === typeof fetchedPassword) {
+							// user does not exist
+							// lets create a new one.
+
+							admin.database().ref(`users/${username}`).set(password);
+
+							// now alert user that their account has been created
+						}
+						else if ('string' === typeof fetchedPassword) {
+							// a user already exists
+							// alert the user to change username
+						}
+					});
+				}
+				else {
+					// alert the user the passwords are not the same
+				}
+			}
+			else {
+				$('#password-fileds').append('<input type=password id=confirm-password placeholder=\'Confirm Password *\'>');
+			}
+		}
+	}
 
 });
--- src/main.js
+++ src/main.js
@@ -1,47 +1,46 @@
 // declearing electron modules
-const electron = require("electron");
+const electron = require('electron');
 const { app, BrowserWindow } = electron;
-const path = require("path");
-const url = require("url");
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
-function createWindow ()
-{
-    // create the browser window option
-    let bwOption = {
-        // transparent makes the title bar transparent
-        // other wise title bar will take system default color
-        // transparent: true,
-        width: 300,
-        minWidth: 300,
-        // maxWidth: 300,
-   
-        height: 450,
-        minHeight: 450
-        // maxHeight: 450
-        // useContentSize: true
-    };
+function createWindow() {
+  // create the browser window option
+  let bwOption = {
+    // transparent makes the title bar transparent
+    // other wise title bar will take system default color
+    // transparent: true,
+    width: 300,
+    minWidth: 300,
+    // maxWidth: 300,
 
-    // Create the browser window.
-    win = new BrowserWindow( bwOption );
+    height: 450,
+    minHeight: 450
+    // maxHeight: 450
+    // useContentSize: true
+  };
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file:",
-        slashes: true
-    }));
+  // Create the browser window.
+  win = new BrowserWindow(bwOption);
 
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null
-    });
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  });
 }
 
 // This method will be called when Electron has finished
@@ -51,19 +50,18 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin')
-    {
-        app.quit()
-    }
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 });
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null)
-        createWindow();
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null)
+    createWindow();
 });
 
 // In this file you can include the rest of your app's specific main process
@@ -72,5 +70,5 @@ app.on('activate', () => {
 // set the menu bar to null when the browser window is created
 // so we have a simless effect
 app.on('browser-window-created', (e, window) => {
-    // window.setMenu(null);
+  // window.setMenu(null);
 });
